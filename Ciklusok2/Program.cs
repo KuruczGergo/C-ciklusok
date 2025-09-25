1a3abf1657dab5d6eb4f932dab57eeff4d4b0d0f
KuruczGergo <kuruczg@kkszki.hu>
2025-09-25 08:38:35 +0200

    Feladat3 is ready

--- Ciklusok2/Program.cs
+++ Ciklusok2/Program.cs
@@ -13,9 +13,10 @@ namespace Ciklusok2
 
         //1 feladat
 
-            Console.Write("Kérem az első számot");
+            Console.Write("Kérem az első számot: ");
             int szam1 = int.Parse(Console.ReadLine());
-            Console.Write("Kérem a második számot");
+            Console.Write("Kérem a második számot: " +
+                "");
             int szam2 = int.Parse(Console.ReadLine());
             
             if (szam1 < szam2)
@@ -33,6 +34,70 @@ namespace Ciklusok2
 
             Console.WriteLine("-----------------------------------------------------");
             //Feladat 2
+
+
+            /*
+            Console.Write("Adjon meg egy számot");
+            int szam = int.Parse(Console.ReadLine());
+            int darab = 0;
+            List<int> szamok = new List<int>();
+            int szam;
+            while (szam1 != 0)
+            {
+                Console.Write("Adjon meg egy számot");
+                szam = int.Parse(Console.ReadLine());
+            }
+            */
+
+            List<int> szamok = new List<int>();
+            int szam;
+
+            while (true)
+            {
+                Console.Write("Adjon meg egy számot: ");
+                szam = int.Parse(Console.ReadLine());
+                if (szam == 0) break;
+                szamok.Add(szam);
+            }
+
+            int maxSzam = 0, maxDb = 0;
+            for (int i = 0; i < szamok.Count; i++)
+            {
+                int db = 1;
+                for (int j = i + 1; j < szamok.Count; j++)
+                    if (szamok[i] == szamok[j]) db++;
+
+                if (db > maxDb)
+                {
+                    maxDb = db;
+                    maxSzam = szamok[i];
+                }
+            }
+
+            if (szamok.Count > 0)
+                Console.WriteLine($"A legtöbbször megadott szám: {maxSzam} ({maxDb} alkalommal)");
+            
+            Console.WriteLine("-----------------------------------------------------");
+            //Feladat 3
+
+            
+
+            List<int> szamok2 = new List<int>();
+            int paros = 0, paratlan = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Kérem a {i+1} számot: ");
+                int szam3 = int.Parse(Console.ReadLine());
+                szamok2.Add(szam3);
+                if (szam3 % 2 == 0) paros++;
+                else paratlan++;
+            }
+            Console.WriteLine($"Páros számok: {paros}, Páratlan számok: {paratlan}");
+
+
+
+
+
 
 
 
